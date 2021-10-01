58f8afa311974da3a173a047eb70e9db83450f98
Christian <chibuike4jamb@gmail.com>
2021-10-01 21:53:39 +0100

    Interview Phase 1...update 5

--- lib/pre_screens/onboarding_screen.dart
+++ lib/pre_screens/onboarding_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/painting.dart';
+import 'package:thinkpeople/pre_screens/register_screen.dart';
 
 class Onboarding extends StatefulWidget {
   const Onboarding({Key? key}) : super(key: key);
@@ -64,31 +65,127 @@ class _OnboardingState extends State<Onboarding> {
               top: MediaQuery.of(context).size.height * 0.6,
               right: MediaQuery.of(context).size.width * 0.0125,
               left: MediaQuery.of(context).size.width * 0.0125,
+              bottom: 0.0,
               child: SizedBox(
                 child: Center(
                   child: Column(
                     children: [
-                      Text(""),
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.65,
-                        height: 15.0,
-                        child: const FittedBox(
-                          fit: BoxFit.fill,
-                          child: Text("Discover New Places", style: TextStyle(
-                            color: Color(0xFF3D8F30),
-                            letterSpacing: 4.5,
-                            fontSize: 13,
+                      Column(
+                        children: [
+                          const Text(""),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.65,
+                            height: 15.0,
+                            child: const FittedBox(
+                              fit: BoxFit.fill,
+                              child: Text("Discover New Places",
+                                style: TextStyle(
+                                color: Color(0xFF3D8F30),
+                                letterSpacing: 4.5,
+                                fontSize: 13,
 
-                            fontWeight: FontWeight.w800,
-                          ),),
+                                fontWeight: FontWeight.w800,
+                              ),
+                              ),
+                            ),
+                          ),
+                          const Text(" "),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.4,
+                              height: 12.0,
+                              child: const FittedBox(
+                                  fit: BoxFit.fill,
+                                  child: Text("Vulputate vitae",
+                                    style: TextStyle(
+                                      color: Color(0xFF3D8F30),
+                                      letterSpacing: 5.0,
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ))),
+
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.4,
+                              height: 12.0,
+                              child: const FittedBox(
+                                  fit: BoxFit.fill,
+                                  child: Text("enim.Vulputate",
+                                    style: TextStyle(
+                                      color: Color(0xFF3D8F30),
+                                      letterSpacing: 5.0,
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ))),
+                          const SizedBox(
+                            height: 8,
+                          ),
+
+                          SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.15,
+                              height: 12.0,
+                              child: const FittedBox(
+                                  fit: BoxFit.fill,
+                                  child: Text("vitae",
+                                    style: TextStyle(
+                                      color: Color(0xFF3D8F30),
+                                      letterSpacing: 5.0,
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ))),
+                        ],
+                      ),
+                      const Spacer(),
+                      InkWell(
+                        onTap: (){
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return const Register();
+                              },
+                            ),
+                          );
+                        },
+                        child: Container(
+                          // child: Container(
+                          height: 60,
+                          width: MediaQuery.of(context).size.width*0.85,
+                          decoration: const BoxDecoration(
+                              color: Color(0xFF3D8F30),
+                              borderRadius: BorderRadius.all(Radius.circular(10)),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Color(0xFF9AFE8A),
+                                    blurRadius: 10,
+                                    offset: Offset(10, 10))
+                              ]),
+
+                          child: Center(
+                            child: SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.4,
+                              height: 15,
+                              child: const FittedBox(
+                                fit: BoxFit.fill,
+                                child: Text(
+                                  "GET STARTED",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    // fontWeight: FontWeight.bold,
+                                    fontSize: 13,
+                                    letterSpacing: 5.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
                         ),
                       ),
-                      Text(" "),
-                      SizedBox(
-
-                          child: Text("Vulputate vitae")),
-                      Text("enim.Vulputate vitae"),
-                      Text("vitae"),
+                      const Spacer(),
                     ],
                   ),
                 ),
